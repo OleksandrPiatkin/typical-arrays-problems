e05812e0512d1d32d3db5da49372c933f56fb795
oleksandrpiatkin <Oleksandr.Piatkin@gmx.com>
2021-02-09 22:01:15 +0100

    feat: add solution for min, max & avg functions

--- src/index.js
+++ src/index.js
@@ -1,12 +1,23 @@
-
 exports.min = function min (array) {
+  if (typeof array !== "undefined" && array.length > 0) {
+    const sorted = array.sort((a, b) => a - b);
+    return sorted[0];
+  }
   return 0;
 }
 
 exports.max = function max (array) {
+  if (typeof array !== "undefined" && array.length > 0) {
+    const sorted = array.sort((a, b) => b - a);
+    return sorted[0];
+  }
   return 0;
 }
 
 exports.avg = function avg (array) {
+  if (typeof array !== 'undefined' && array.length > 0) {
+    const sum = array.reduce((total, val) => total + val);
+    return sum / array.length;
+  }
   return 0;
 }
